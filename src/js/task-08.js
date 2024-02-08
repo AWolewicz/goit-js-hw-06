089dec720bc4c4a618ae141fe6deb408e718f866
AWolewicz <agnieszkawolewicz5@gmail.com>
2024-02-08 08:59:27 +0100

    update

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -2,8 +2,8 @@ const loginForm = document.querySelector('.login-form');
 
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  const emailForm = loginForm.elements.email.value
-  const passwordForm = loginForm.elements.password.value
+  const emailForm = loginForm.elements.email.value.trim();
+  const passwordForm = loginForm.elements.password.value.trim();
   const loginObject = {
     email: emailForm,
     password: passwordForm
